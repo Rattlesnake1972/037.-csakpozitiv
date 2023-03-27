1f9d1ac20163cc2fa9fbff633dc0122441a284bf
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 14:57:18 +0200

    Csak pozitív számokat hajlandó kiírni

---
+++ 037. csakpozitiv/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _037.csakpozitiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strLine;
+            int intA;
+            do
+            {
+                Console.Clear();
+                Console.Write("Kérek egy pozitív számot, mert negatívakat nem vagyok hajlandó kiírni! ");
+                strLine = Console.ReadLine();
+                intA = Convert.ToInt32(strLine);
+                if (intA <= 0)
+                {
+                    Console.WriteLine("Pozitív számot írj be!");
+                }
+            } while (intA > 0);
+
+            Console.ReadLine();
+
+        }
+    }
+}
